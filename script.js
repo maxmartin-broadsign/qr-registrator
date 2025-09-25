c472ac1fb23422e56a7858df8f5069b070ad5c83
Maximilien Martin <maximilien.martin@broadsign.com>
2025-09-25 13:41:41 -0400

    Fetch active registration through BSS API

--- script.js
+++ script.js
@@ -7,22 +7,70 @@ document.addEventListener('DOMContentLoaded', () => {
         const inputValue = inputField.value.trim();
 
         if (inputValue !== '') {
-            validateInput(inputValue);
-            inputField.value = '';
+            resultsSection.innerHTML = '';
+            getClientRegistration(inputValue);
         } else {
             alert('Please enter some text before validating.');
         }
     });
 
-    function validateInput(value) {
+    async function getClientRegistration(restApiKey) {
+        let url = "http://dev.broadsign.com/rest/client_registration/v7/";
+        try {
+            const httpCall = fetch(url, {
+                mode: 'cors',
+                headers: {
+                    'Authorization': `Bearer ${restApiKey}`,
+                }
+            })
+            
+            // Wait for the response from the server.
+            const response = await httpCall;
+
+            if (!response.ok) {
+                displayError("Invalid REST API key provided.");
+                return;
+            }
+
+            // Parse the JSON data from the response.
+            const data = await response.json();
+            
+            if (data && data.client_registration && Array.isArray(data.client_registration))
+            {
+                const client_registration = data.client_registration.filter(registration => 
+                    { return registration.active && registration.custom_unique_id; });
+
+                client_registration.forEach(registration => {
+                    displayRegistrations(registration)
+                })
+            }
+
+        } catch (error) {
+            console.error('Fetch error:', error);
+            displayError(`Invalid API token`);
+        }
+    }
+
+    function displayRegistrations(activeRegistration)
+    {
         const newButton = document.createElement('button');
-        newButton.className = 'result-button';
-        newButton.textContent = `Result: ${value}`;
+        newButton.className = 'registrations-button';
+        newButton.textContent = `${activeRegistration.custom_unique_id}`;
 
         newButton.addEventListener('click', () => {
-            alert(`You clicked: "${value}"`);
+            alert(`You clicked: "${activeRegistration.custom_unique_id}"`);
         });
 
         resultsSection.appendChild(newButton);
+    }
+
+    function displayError(errorMessage)
+    {
+        let newElement;
+        newElement = document.createElement('div');
+        newElement.className = 'result-text error-message';
+        newElement.textContent = errorMessage;
+
+        resultsSection.appendChild(newElement)
     }
 });
